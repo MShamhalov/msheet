6605398c3c46e431975c5f7bd1fca87ac1024770
MShamhalov <m@shamhalov.ru>
2021-06-25 08:05:39 +0300

    Базовые принципы

--- src/msheet.php
+++ src/msheet.php
@@ -1,6 +1,38 @@
 <?php
-phpinfo();
-echo("Hello World");
+$string = file_get_contents("msheet.json");
+$shipments = json_decode($string, true);
+$table = $_GET['sheet'];
 
+if(isset($shipments[$table])){
+	foreach($shipments[$table] as $current){
+		if(gettype($current) == 'array'){
+			foreach($current as $cuser){
+				echo $cuser . " ";
+			}
+		}
+		else echo $current . " ";
+	}
 
+	
+echo '<table class="table table-hover table-sm table-striped">';
+echo '<thead>';
+echo '<tr>';
+	//foreach($shipments['columns'] as $current){
+	//	echo "<th>$current</th>";
+	//}
+echo '</tr>';
+
+echo '<tr>';
+	//foreach($shipments['columns'] as $current){
+	//	echo "<th>$current</th>";
+	//}
+echo '</tr>';
+
+echo '<tbody>';
+echo '</table>';
+	}
+else {
+	echo 'Нет формы отображения для текущей таблицы <br /><br />';
+	echo 'Тут должен быть код, который самостоятельно анализирует таблицу, и если она существует в БД, создает для ее форму Default (с именем таблицы как в БД)';
+}
 ?>
