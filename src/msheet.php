8e7d96af5abcd8e2782a692b771810c1559efaeb
MShamhalov <m@shamhalov.ru>
2021-07-15 08:46:51 +0300

    Наведение порядка в работе компонента

--- src/msheet.php
+++ src/msheet.php
@@ -1,38 +1,49 @@
 <?php
-$string = file_get_contents("msheet.json");
+$string = file_get_contents("config.json");
 $shipments = json_decode($string, true);
-$table = $_GET['sheet'];
+$table = $_GET['component'];
 
+if (isset($_GET['view']))
+	$view = $_GET['view'];
+else $view = 'default';
+
+
+
+//echo $shipments["user"]["name"] . "<br/>";
+
+
+
+echo '<table class="table table-hover table-sm table-striped">';
+echo '<thead>';
+	//Перебор строк
+	echo '<th>'; 
+	//Перебор полей
+	foreach($shipments["user"]['views'][$view] as $current){
+		echo '<td>'; 
+			echo $current. "<br />";
+		echo '</td>';
+	}
+	echo '</th>'; 
+echo '</thead>';
+echo '<tbody>';
+
+echo '</tbody>';
+echo '</table>';
+/*
 if(isset($shipments[$table])){
 	foreach($shipments[$table] as $current){
 		if(gettype($current) == 'array'){
 			foreach($current as $cuser){
-				echo $cuser . " ";
+				/*
+				if(gettype($cuser) == 'array'){
+					foreach($cuser as $cdata){
+						echo $cdata . " ";
+					}
+				}
+				
 			}
 		}
-		else echo $current . " ";
+		//else echo $current . " ";
 	}
-
-	
-echo '<table class="table table-hover table-sm table-striped">';
-echo '<thead>';
-echo '<tr>';
-	//foreach($shipments['columns'] as $current){
-	//	echo "<th>$current</th>";
-	//}
-echo '</tr>';
-
-echo '<tr>';
-	//foreach($shipments['columns'] as $current){
-	//	echo "<th>$current</th>";
-	//}
-echo '</tr>';
-
-echo '<tbody>';
-echo '</table>';
-	}
-else {
-	echo 'Нет формы отображения для текущей таблицы <br /><br />';
-	echo 'Тут должен быть код, который самостоятельно анализирует таблицу, и если она существует в БД, создает для ее форму Default (с именем таблицы как в БД)';
-}
+*/
 ?>
