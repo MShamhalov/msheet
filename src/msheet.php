3fe617f711a8cfb457e7cfb7def3bfd5706579a7
MShamhalov <m@shamhalov.ru>
2021-07-16 09:35:02 +0300

    Работают view-ы, отображение 15 элементов таблицы

--- src/msheet.php
+++ src/msheet.php
@@ -1,4 +1,9 @@
 <?php
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">';
+echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>';
+
+require_once '../../db_connect.php';
+
 $string = file_get_contents("config.json");
 $shipments = json_decode($string, true);
 $table = $_GET['component'];
@@ -7,43 +12,28 @@ if (isset($_GET['view']))
 	$view = $_GET['view'];
 else $view = 'default';
 
-
-
-//echo $shipments["user"]["name"] . "<br/>";
-
-
-
+$query_main = "SELECT * FROM sys_user LIMIT 0, 15;";
+$result_main = $pdo->query($query_main);
+echo '<div class="main_frame">';
 echo '<table class="table table-hover table-sm table-striped">';
 echo '<thead>';
-	//Перебор строк
-	echo '<th>'; 
 	//Перебор полей
 	foreach($shipments["user"]['views'][$view] as $current){
-		echo '<td>'; 
-			echo $current. "<br />";
-		echo '</td>';
+		echo '<th>'; 
+			echo $current;
+		echo '</th>';
 	}
-	echo '</th>'; 
+
 echo '</thead>';
 echo '<tbody>';
-
+		foreach($result_main  as $current){
+			echo '<tr>';
+			foreach ($shipments["user"]['views'][$view] as $ctd){
+				echo '<td>' . $current[$ctd] . '</td>';
+			}
+			echo '</tr>';
+		}
 echo '</tbody>';
 echo '</table>';
-/*
-if(isset($shipments[$table])){
-	foreach($shipments[$table] as $current){
-		if(gettype($current) == 'array'){
-			foreach($current as $cuser){
-				/*
-				if(gettype($cuser) == 'array'){
-					foreach($cuser as $cdata){
-						echo $cdata . " ";
-					}
-				}
-				
-			}
-		}
-		//else echo $current . " ";
-	}
-*/
+echo '</div>'
 ?>
