71357de9c57f0f8c1bba51b9ee00d69d5a9cfb74
MShamhalov <m@shamhalov.ru>
2021-09-21 08:25:06 +0300

    Исправление несущественных багов

--- src/msheet.php
+++ src/msheet.php
@@ -2,12 +2,18 @@
 echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">';
 echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>';
 
+echo '<div class="">';
+echo'<link rel="stylesheet" type="text/css" href="style.css" />';
+echo '</div>';
+
 require_once '../../db_connect.php';
 
 $string = file_get_contents("config.json");
 $shipments = json_decode($string, true);
-$table = $_GET['component'];
-
+if (isset($_GET['component']))
+	$table = $_GET['component'];
+else header('Location: ./404.html'); 
+	
 if (isset($_GET['view']))
 	$view = $_GET['view'];
 else $view = 'default';
@@ -15,10 +21,13 @@ else $view = 'default';
 $query_main = "SELECT * FROM sys_user LIMIT 0, 15;";
 $result_main = $pdo->query($query_main);
 echo '<div class="main_frame">';
+
+echo "<label class='tableLabel'>$table</label>";
+
 echo '<table class="table table-hover table-sm table-striped">';
 echo '<thead>';
 	//Перебор полей
-	foreach($shipments["user"]['views'][$view] as $current){
+	foreach($shipments[$table]['views'][$view] as $current){
 		echo '<th>'; 
 			echo $current;
 		echo '</th>';
@@ -28,7 +37,7 @@ echo '</thead>';
 echo '<tbody>';
 		foreach($result_main  as $current){
 			echo '<tr>';
-			foreach ($shipments["user"]['views'][$view] as $ctd){
+			foreach ($shipments[$table]['views'][$view] as $ctd){
 				echo '<td>' . $current[$ctd] . '</td>';
 			}
 			echo '</tr>';
